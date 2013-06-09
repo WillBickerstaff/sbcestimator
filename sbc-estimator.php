810616bd4462c32e2daed0c15c90a136b49855ce
Will Bickerstaff <will.bickerstaff@gmail.com>
2013-06-09 12:38:55 +0100

    Plugin header added

---
+++ sbc-estimator.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: SBC Estimator
+Plugin URI: http://github.com/WillBickerstaff/sbcestimator
+Description: A building cost estimaor for http://www.stablebuildcompany.co.uk
+Version: 1.0a
+Author: Will Bickerstaff
+License: GPLv3
+*/
+
+/*  Copyright 2013 Will Bickerstaff (email: will.bickerstaff@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
