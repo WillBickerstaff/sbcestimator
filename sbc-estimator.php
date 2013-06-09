d9461c8f7ec0043cf3a3e29080648567395aaccd
Will Bickerstaff <will.bickerstaff@gmail.com>
2013-06-09 13:29:43 +0100

    Initial install code

--- sbc-estimator.php
+++ sbc-estimator.php
@@ -23,4 +23,54 @@ License: GPLv3
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+global $sbc_estimator_db_version;
+$sbc_estimator_db_version = "1.0";
+
+register_activation_hook( __FILE__, 'sbc_estimator_install');
+
+add_action( 'admin_menu', 'sbc_plugin_menu' );
+
+function sbc_plugin_menu() {
+  add_options_page('Estimator Options', 'SBC Estimator', 'manage_options', 'sbcestimator', 'sbc_options');
+}
+
+function sbc_options() {
+
+}
+
+function sbc_estimator_install () {
+  global $wpdb;
+  global $sbc_estimator_db_version;
+
+  $tablename = $wpdb->prfix . "sbc_estimator_datatypes";
+
+  require_once( ABSPATH . "wp-admin/includes/upgrade.php" );
+
+  $sql = "CREATE TABLE $tablename (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    name tinytext NOT NULL,
+    PRIMARY KEY id (id)
+  );";
+  
+  dbDelta ( $sql );
+
+  $rows_affected = $wpdb->insert( $tablename, array( 'name' => 'building', 'name' => 'option' ) );
+
+  $tablename = $wpdb->prefix . "sbc_estimator_data";
+
+  $sql = "CREATE TABLE $tablename (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    type mediumint NOT NULL,
+    name tinytext NOT NULL,
+    description text,
+    units tinytext NOT NULL,
+    price_unit DOUBLE NOT NULL,
+    PRIMARY KEY id (ID)
+  );";
+
+  dbDelta ( $sql );
+
+  add_option ('sbc_estimator_db_version', $sbc_estimator_db_version );
+}
 ?>
